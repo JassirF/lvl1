c41f5f3e481eed45d9466739ed19166e6e9dad31
jassir <jassir.f95@gmail.com>
2023-07-21 21:57:14 +0200

    Primera version

---
+++ pythagorean-theorem.py
@@ -0,0 +1,33 @@
+"""Create a hypotenuse.py program that asks the user for two numbers, a and b, and calculates the hypotenuse using the Pythagorean equation, rewritten like so:
+
+c**2= sqr.root(a**2 + b**2)
+"""
+import math
+print('Bienvenido al teorema de pitagoras, te va petar el cerebro')
+print('Elige 2 de las variables que me daras, puede ser cualquiera, PERO DEBEN SER DOS!')
+
+
+a = (input('a = '))
+b = (input('b = '))
+c = (input('c = '))
+while True:
+    if a == '':
+        b = int(b)
+        c = int(c)
+        print('Calculare a')
+        a = math.sqrt((b**2+c**2))
+        print('Esta es c:', a)
+    if b == '':
+        a = int(a)
+        c = int(c)
+        print('Calculare b')
+        b = math.sqrt((a**2+c**2))
+        print('Esta es c:', b)
+    if c == '':
+        a = int(a)
+        b = int(b)
+        print('Calculare c')
+        c = math.sqrt((a**2+b**2))
+        print('Esta es c:', c)
+    else:
+        break
