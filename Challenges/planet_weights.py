f84f2f78c4fc70ba5131a8d22750f296ba3a97d4
jassir <jassir.f95@gmail.com>
2023-07-23 14:59:56 +0200

    1.0

--- Challenges/planet_weights.py
+++ Challenges/planet_weights.py
@@ -25,22 +25,22 @@ planet_id = int(input('Planet ID: '))
 
 if planet_id == 1:
     d_weight = e_weight*Mercury
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
 elif planet_id == 2:
     d_weight = e_weight*Venus
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
 elif planet_id == 3:
     d_weight = e_weight*Mars
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
 elif planet_id == 4:
     d_weight = e_weight*Jupiter
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
 elif planet_id == 5:
     d_weight = e_weight*Saturn
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
 elif planet_id == 6:
     d_weight = e_weight*Uranus
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
 elif planet_id == 7:
     d_weight = e_weight*Neptune
-    print('Your package weight at destination is: ', d_weight)
+    print('Your package weight at destination is:', d_weight)
